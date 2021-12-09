c97754f00d4ad99fed796f713e91be8549437b0f
mkilianski <m.d.kilianski@gmail.com>
2021-12-09 17:56:35 +0100

    Init parallel

--- main.cpp
+++ main.cpp
@@ -2,26 +2,37 @@
 #include <fstream>
 #include <vector>
 #include <chrono>
+#include <stdlib.h>
+#include <omp.h>
 
 int main() {
-
-    std::fstream file;
+	std::fstream file;
     std::string word;
-    auto filename = "./amazon_reviews.txt";
+	auto filename = "./test.ft.txt";
+	file.open(filename);
+	std::vector<std::string> words;	
+	int total = 0; //suma zliczonych slow przez wszystkie watki
 
-    file.open(filename);
-    std::vector<std::string> words;
-
-    auto start = std::chrono::high_resolution_clock::now();
-
-    while (file >> word)
+	while (file >> word)
     {
-        words.push_back(word);
-    }
+	  	words.push_back(word);
+	}
+	int size = words.size();
+	std::cout << "size of " << size << std::endl;
+	auto start = std::chrono::high_resolution_clock::now();
+	int words_size = words.size(); //zeby for bylo w kanonicznej postaci
+	omp_set_num_threads(2); 
+	#pragma omp parallel shared(words_size)
+	{
+		int k=0; //for jest taka pusta operacja, chodzi o same wykorzystanie podzialu na watki
+		#pragma omp for
+	  		for (int i = 0; i < words_size ; i++) {k += 1;}
+		total+=k;
+	}
+	auto end = std::chrono::high_resolution_clock::now();
+	std::cout << "Elapsed: " << std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count() << std::endl;
+	std::cout << "words.size(): " << words.size() << std::endl;
 
-    auto end = std::chrono::high_resolution_clock::now();
-    std::cout << "Elasped: " << std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count() << std::endl;
-    std::cout << "Vec count: " << words.size() << std::endl;
-
+    std::cout << "TOTAL_COUNTED: " << total << std::endl;
     return 0;
-}
+} 
