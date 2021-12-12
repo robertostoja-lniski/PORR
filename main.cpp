a15d5013e58af80f9ffd338d4765e972c7eb166f
mkilianski <m.d.kilianski@gmail.com>
2021-12-12 16:23:35 +0100

    zliczanie slow w wersji rownoleglej

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <chrono>
 #include <stdlib.h>
 #include <omp.h>
+#include <map>
 
 int main() {
 	std::fstream file;
@@ -11,28 +12,35 @@ int main() {
 	auto filename = "./test.ft.txt";
 	file.open(filename);
 	std::vector<std::string> words;	
-	int total = 0; //suma zliczonych slow przez wszystkie watki
 
-	while (file >> word)
-    {
+	while (file >> word){
 	  	words.push_back(word);
 	}
 	int size = words.size();
 	std::cout << "size of " << size << std::endl;
 	auto start = std::chrono::high_resolution_clock::now();
 	int words_size = words.size(); //zeby for bylo w kanonicznej postaci
-	omp_set_num_threads(2); 
+	omp_set_num_threads(5); 
+
+	std::map<std::string, int> map;
 	#pragma omp parallel shared(words_size)
 	{
-		int k=0; //for jest taka pusta operacja, chodzi o same wykorzystanie podzialu na watki
 		#pragma omp for
-	  		for (int i = 0; i < words_size ; i++) {k += 1;}
-		total+=k;
+	  		for (int i = 0; i < words_size ; i++) {
+	  			map[words[i]]+=1;
+	  		}
 	}
 	auto end = std::chrono::high_resolution_clock::now();
-	std::cout << "Elapsed: " << std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count() << std::endl;
+	
+	int k = 0;
+	for(auto const& x : map){
+		std::cout << k << ": " << x.first << " " << x.second << std::endl;
+		k++;
+		if(k>500)//wypisanie pierwszych elementow mapy, tylko do testu czy
+			break;
+	}
 	std::cout << "words.size(): " << words.size() << std::endl;
-
-    std::cout << "TOTAL_COUNTED: " << total << std::endl;
+    std::cout << "Elapsed: " << std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count() << std::endl;
+    
     return 0;
 } 
